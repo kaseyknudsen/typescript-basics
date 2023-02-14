8007478b042e648134549ab46342784e39d023fd
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-02-14 10:15:26 -0800

    optional types

--- project-files/2-working-with-types/2-object-types-begin/objectTypes.ts
+++ project-files/2-working-with-types/2-object-types-begin/objectTypes.ts
@@ -3,7 +3,7 @@ to have the type alias start with a capital letter */
 type Game = {
   title: string;
   genres: string[];
-  price: number;
+  price?: number;
 };
 
 let game: Game = {
@@ -12,15 +12,22 @@ let game: Game = {
   price: 35,
 };
 
-let gameTwo: {
-  title: string;
-  genres: string[];
-} = {
+let gameTwo: Game = {
   title: "Super mario Sunshine",
   genres: ["platformer", "adventure"],
 };
 
 console.log(game.price);
+
+
+let games: Game[] = [game, gameTwo];
+
+//nullish coalescing operator. Use this in case there is no price in one of the objects
+for (const game of games) {
+  //console.log((game.price ?? 0) * 100);
+  //optional chaining--add a question mark after price
+  console.log(game.price?.toString())
+}
 
 type Bike = {
   brand: string;
@@ -52,3 +59,20 @@ let dog1: Dog = {
 console.log(bike.brand);
 console.log(`My dog's name is ${dog1.name}. Hi is a ${dog1.breed}, his color is ${dog1.color}, and
 his size is ${dog1.size[1]}. He was born in ${dog1["year born"]}.`);
+
+// 2 types of optional types in typescript: parameters & properties. Signifies that the property may or may not exist
+//optional properties: add a question mark at end
+type Shoes = {
+  brand: string;
+  model: string;
+  year: number;
+  size?: string;
+};
+
+let runningShoes: Shoes = {
+  brand: "Brooks",
+  model: "Ghost",
+  year: 2022,
+};
+
+console.log(runningShoes.brand);
