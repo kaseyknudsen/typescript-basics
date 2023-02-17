1fd4c6fd00df6746770a0d27f25f934140df0bf6
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-02-17 08:40:25 -0800

    type narrowing

--- project-files/4-functions-in-ts/1-type-annotations-begin/parameter-annotations.ts
+++ project-files/4-functions-in-ts/1-type-annotations-begin/parameter-annotations.ts
@@ -1,10 +1,42 @@
-const language: string = 'ts'
+type Employee = {
+  name: string;
+  job: string;
+};
+
+type Manager = {
+  name: string;
+  manages: Employee[];
+};
 
 /* this is telling typescript that both num1 & num2 should be numbers and the
 function should return a number */
 
-type NumOrString = number | string
+type NumOrString = number | string;
 
-function add(num1: number, num2: number, num3?: NumOrString): number {
-    return num1 + num2
+// function add(num1: number, num2: number, num3?: NumOrString): number {
+//     return num1 + num2
+// }
+
+function add(num1: number, num2: NumOrString): number {
+  if (typeof num2 === "number") {
+    return num1 + num2;
+  }
+
+  return num1 + Number(num2);
+}
+
+//type narrowing is a way of narrowing down the exact type of our variable
+
+//this is 'truthiness narrowing'
+function subtract(num1: number, num2?: number): number {
+  if (num2) {
+    return num1 - num2;
+  }
+  return num1 + Number(num2);
+}
+
+function job(person: Employee | Manager) {
+  if ("job" in person) {
+    return person.job;
+  } return "This person doesn't have a job."
 }
